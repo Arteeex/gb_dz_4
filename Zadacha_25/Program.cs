9b84227b04b4323932916d616d9061b246e131a4
Arteeex <arteeex187@gmail.com>
2023-01-25 15:19:45 +0700

    Zadacha_15

--- Zadacha_25/Program.cs
+++ Zadacha_25/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int powpow(int a, int b)
+{
+    int result = Convert.ToInt32(Math.Pow(a,b));
+    return result;
+}
+
+Console.Write("Введите число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+int result = powpow(value1, value2);
+Console.WriteLine("Ответ: " + result);
